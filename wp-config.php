36bb750a5c63356d1d01ddd2d22928bf921f39ee
Ljubica Kostova <kostova_ljubica@hotmail.com>
2016-03-14 21:58:56 +0100

    new
    new db config

--- wp-config.php
+++ wp-config.php
@@ -21,16 +21,17 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 #define('DB_NAME', 'wordpress');
-define('DB_NAME', 'daukr9e15sb4dj');
+define('DB_NAME', 'sql7110707');
 
 /** MySQL database username */
-define('DB_USER', 'rflmrxmxqnhtut');
+define('DB_USER', 'sql7110707');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'WmODf3QYpZEdqT4Vi0qGJ6tXNZ');
+define('DB_PASSWORD', 'mqe2quDZC2');
 
 /** MySQL hostname */
-define('DB_HOST', 'ec2-46-137-125-22.eu-west-1.compute.amazonaws.com');
+// define('DB_HOST', 'ec2-46-137-125-22.eu-west-1.compute.amazonaws.com');
+define('DB_HOST', 'sql7.freemysqlhosting.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
